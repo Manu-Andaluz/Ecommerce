36628adfb7766fa158c742a6df87cca48d040cfd
manu-andaluz <manupeandaluz@gmail.com>
2023-01-13 00:41:51 -0300

    adding new url (railway)

--- frontend/src/slices/api.js
+++ frontend/src/slices/api.js
@@ -1,4 +1,4 @@
-export const url = "http://localhost:5000/api";
+export const url = "https://ecommerce-backend-production-be36.up.railway.app/";
 
 export const setHeaders = () => {
   const headers = {
